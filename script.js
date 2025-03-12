2b6186703bec64219c9a0e8893e484cd4ded55c1
unknown <abuthahirchalissery162@gmail.com>
2025-03-12 14:23:57 +0530

    task add & delete added

--- script.js
+++ script.js
@@ -1,17 +1,71 @@
+// Selecting elements
+const todoInput = document.getElementById('input-todo');
+const addBtn = document.getElementById('add-btn');
+const listContainer = document.getElementById('list-container');
 
-let TodoInput = document.getElementById('input-todo')
 
-let AddBtn = document.getElementById('add-btn')
+// Add Task Function
+function addTask(e) {
+    e.preventDefault();
 
-let ListContainer = document.getElementById('list-container')
+    const task = todoInput.value.trim(); 
 
-function addTask(){
-    let TodoLi = document.createElement('li')
-    TodoLi.textContent = TodoInput.value
-    ListContainer.appendChild(TodoLi)
-    TodoInput.value = ""
+    if (task === "") return;
 
+    // Creating task elements
+    const todoDiv = document.createElement('div');
+    const todoLi = document.createElement('li');
+    const checkBox = document.createElement('button')
+    const delDiv = document.createElement('div');
+    const trashBtn = document.createElement('button');
+    const editBtn = document.createElement('button');
     
+
+    // Adding content
+    todoLi.textContent = task;
+    editBtn.innerHTML = `<ion-icon class="text-lg text-[#4A4D57]" name="create-outline"></ion-icon>`;
+    trashBtn.textContent = `<ion-icon class="text-[#4A4D57] text-lg" name="trash-outline"></ion-icon>`;
+
+
+    // Appending elements
+    todoDiv.appendChild(todoLi);
+    todoLi.prepend(checkBox)
+    delDiv.appendChild(editBtn)
+    delDiv.appendChild(trashBtn);
+    todoDiv.appendChild(delDiv);
+    listContainer.appendChild(todoDiv);
+
+
+
+
+
+    // Input field clearing
+    todoInput.value = "";
+
+    // Adding classes
+    todoDiv.classList.add("flex", "justify-between", "items-center", "py-1.5", "my-4", "px-3", "border-1", "rounded-md", "bg-[#1C1D20]", "border-none", "text-gray-50");
+    todoLi.classList.add("flex", "gap-2", "items-center");
+    checkBox.classList.add("w-5", "h-5", "rounded-full",  "border-1",  "text-[#4A90E2]")
+    delDiv.classList.add("flex", "items-center", "justify-center", "gap-2");
+    trashBtn.classList.add("text-red-500", "hover:text-red-700", "transition");
+
+
+    // Adding the icons
+    trashBtn.innerHTML = trashBtn.innerHTML = `<ion-icon class="text-[#4A4D57] text-lg" name="trash-outline"></ion-icon>`;
+
+
+    // Delete Task Functionality
+    function TrashTask(){
+        listContainer.removeChild(todoDiv)
+    }
+    // delete
+    trashBtn.addEventListener('click',TrashTask);
+    
+
+
 }
 
-AddBtn.addEventListener('click',addTask)
+
+
+//  adding tasks
+addBtn.addEventListener('click', addTask);
